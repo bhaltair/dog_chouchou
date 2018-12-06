01dcbc9bb878fc8af7054817ac84595d9d8fe941
wangqi <wangqi@hemayun.com>
2018-12-06 17:39:53 +0800

    update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/chouchou/dist',
+    assetsPublicPath: '/dist',
 
     /**
      * Source Maps
