81215440102322285b88a54e4d15075ef69a2fa6
wangqi <njcit_wangqi@163.com>
2020-05-06 17:11:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,46 @@
 <template>
   <div id="app">
-    <h2>丑丑生日 {{ birthday }}</h2>
+    <h2>丑丑</h2>
     <img
-      src="https://static.lovefrontend.com/IMG_2199.jpg"
+      src="https://s1.ax1x.com/2020/05/06/YEKC6g.jpg"
       alt="丑丑"
       width="200"
       srcset=""
     />
-    <h3>到今天</h3>
     <p>
-      已经 <b class="fz20">{{ age }} </b> 天了！
+      生日 {{ birthday }}
     </p>
     <p>
-      第 <b class="fz20">{{ week }} </b> 周了！
+      到今天已经 <b class="fz20">{{ day }} </b> 天了！
+    </p>
+    <p>
+      {{ year }}岁 {{ month }} 个月
     </p>
   </div>
 </template>
 
 <script>
-import { SpringSpinner } from 'epic-spinners';
-import dayjs from 'dayjs';
-import HelloWorld from './components/HelloWorld';
-
+const BIRTHDAY = '2018-09-25';
 export default {
   name: 'App',
   data() {
     return {
-      birthday: dayjs('2018-09-25').format('YYYY-MM-DD'),
-      age: Math.floor(
-        (Date.now() - new Date('2018-09-25').valueOf()) / 86400000,
+      birthday: BIRTHDAY,
+      day: Math.floor(
+        (Date.now() - new Date(BIRTHDAY).valueOf()) / 86400000,
       ),
     };
   },
   computed: {
-    week() {
-      return Math.floor(this.age / 7);
+    year() {
+      return Math.floor(this.day / 365);
     },
-  },
-  components: {
-    HelloWorld,
-    SpringSpinner,
+    month() {
+      return Math.floor((this.day - (this.year * 365)) / 30);
+    },
+    week() {
+      return Math.floor(this.day / 7);
+    },
   },
 };
 </script>
