3f96614800ada75edd503e5ad0fcc89f4162f85c
wangqi <njcit_wangqi@163.com>
2020-05-06 16:30:50 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dog_chouchou',
     proxyTable: {},
 
     // Various Dev Server settings
